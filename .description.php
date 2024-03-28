82777c93b138aa1705024a696ccfcffc7a1a84db
absalyamovd <denis.absalyamov@gmail.com>
2024-03-29 02:13:23 +0300

    1st commit

---
+++ .description.php
@@ -0,0 +1,13 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => "Вывести всех админов",
+	"DESCRIPTION" => "Тестовое задание `Вывести всех админов`",
+	"CACHE_PATH" => "Y",
+	"SORT" => 9999999,
+	"PATH" => array(
+        "ID" => "AllAdminsTestTask",
+        "NAME" => "AllAdminsTestTask",
+	),
+);
